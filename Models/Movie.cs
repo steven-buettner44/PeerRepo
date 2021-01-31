4593f6e8830cfb3b82aff24fc1b2bf729d2bc50c
steven-buettner44 <66680160+steven-buettner44@users.noreply.github.com>
2021-01-31 13:35:49 -0600

    Add project files.

---
+++ Models/Movie.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+namespace PeerSupport.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+
+       public string Name { get; set; }
+
+       [DataType(DataType.Date)]
+       public DateTime Workweek { get; set; }
+
+      public int Hours { get; set; }
+        public int Minutes { get; set; }
+
+        [DataType(DataType.MultilineText)]
+     public string Notes { get; set; }
+    }
+}
